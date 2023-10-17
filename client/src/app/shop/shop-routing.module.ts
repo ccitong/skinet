1f8171a920bfa1884c4f418adc5fcd0fd2d80fca
ccitong <ccitong@gmail.com>
2023-10-16 23:02:47 -0400

    End of Section 9

---
+++ client/src/app/shop/shop-routing.module.ts
@@ -0,0 +1,12 @@
+import { Input, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShopRoutingModule { 
+}
