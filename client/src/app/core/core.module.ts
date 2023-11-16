8fd68e33e32b303525e81973d039845ae73171d4
ccitong <ccitong@gmail.com>
2023-11-16 16:46:30 -0500

    End of Section 17

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -9,6 +9,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { BreadcrumbModule } from 'xng-breadcrumb';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -34,7 +35,8 @@ import { NgxSpinnerModule } from 'ngx-spinner';
   exports:[
     NavBarComponent,
     SectionHeaderComponent,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    SharedModule
   ]
 })
 export class CoreModule { }
